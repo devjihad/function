b90e337a35a87bfa304498cddf941a7bd158e9b2
devjihad <mdhajihad609@gmail.com>
2024-10-05 20:51:56 +0600

    first

--- Problem_3.js
+++ Problem_3.js
@@ -1,1 +1,12 @@
+let array = [11,25,24,36,52,35]
+function make_avg(num){
+    let sum = 0
+    for(let number of num){
+        sum = sum + number
+    }
+    let result = sum/num.length
+    return result
+        
 
+}
+console.log(make_avg(array))
